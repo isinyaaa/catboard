fcd34b42a01f7d9e9130ce9229ef898958a64538
Isabella Basso <isabellabdoamaral@usp.br>
2023-12-29 13:13:57 -0300

    add delete method

--- src/catboard/main.py
+++ src/catboard/main.py
@@ -1,3 +1,5 @@
+from collections import UserList
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -8,21 +10,35 @@ class Thread(BaseModel):
     title: str
     body: str
     image: str
+    _deleted: bool = False
 
 
-threads = []
+class Threads(UserList[Thread]):
+    def exists(self, i) -> bool:
+        if i > len(self.data):
+            return False
+        if self.data[i]._deleted:
+            return False
+        return True
+
+    def pop(self, i=-1) -> Thread:
+        self.data[i]._deleted = True
+        return self[i]
+
+
+threads = Threads()
 
 
 @app.get("/threads/{thread_id}")
 async def read_thread(thread_id: int):
-    if thread_id > len(threads):
-        return {"error": "thread not found"}
-    return threads[thread_id]
+    if threads.exists(thread_id):
+        return threads[thread_id]
+    return {"error": "thread not found"}
 
 
 @app.get("/threads")
 async def read_threads(skip: int = 0, limit: int = 50):
-    return threads[skip : skip + limit]
+    return list(filter(lambda t: not t._deleted, threads))[skip : skip + limit]
 
 
 @app.post("/threads")
@@ -33,7 +49,15 @@ async def create_thread(thread: Thread):
 
 @app.put("/threads/{thread_id}")
 async def update_thread(thread_id: int, thread: Thread):
-    if thread_id > len(threads):
-        return {"error": "thread not found"}
-    threads[thread_id] = thread
-    return {"success": True}
+    if threads.exists(thread_id):
+        threads[thread_id] = thread
+        return {"success": True}
+    return {"error": "thread not found"}
+
+
+@app.delete("/threads/{thread_id}")
+async def delete_thread(thread_id: int):
+    if threads.exists(thread_id):
+        threads.pop(thread_id)
+        return {"success": True}
+    return {"error": "thread not found"}
